1f024182f18f31327cfcd7a609f1c654f4218fe7
nickm <nickmangarella89@gmail.com>
2021-03-04 11:15:37 -0500

    retrived csv and appended axes and circles

--- assets/js/app.js
+++ assets/js/app.js
@@ -70,5 +70,68 @@ function updateToolTip(chosenXAxis, circlesGroup) {
     else {
         label = "Age:"
     }
+
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`${d.abbr}<br>${label} ${d[chosenXAxis]}`);
+        });
+
+        circlesGroup.call(toolTip);
+
+        // On mouseover event
+        circlesGroup.on("mouseover", function(data) {
+            toolTip.show(data);
+        })
+            // On mouseout event
+            .on("mouseout", function(data, index) {
+                toolTip.hide(data);
+            });
+    
+    return circlesGroup;
 }
 
+// Retrieve data from CSV file and execute the chart
+d3.csv("/data/data.csv").then(function(healthData, err) {
+    if (err) throw err;
+
+    // Parse the data
+    healthData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+        data.age = +data.age;
+    });
+
+    // xLinearScale function above csv import
+    var xLinearScale = xScale(healthData, chosenXAxis);
+
+    // Create y scale function
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(healthData, d => d.healthcare)])
+        .range([height, 0]);
+    
+    // Create initial axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Append x-axis
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    // Append y-axis
+    var yAxis = chartGroup.append("g")
+        .classed("y-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(leftAxis);
+    
+    // Append initial circles
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(healthData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d[chosenXAxis]))
+        .attr("cy", d => yLinearScale(d[chosenYAxis]))
+})
